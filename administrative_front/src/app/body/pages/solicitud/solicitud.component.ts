93edd95055f07ac33d335c58d6e4440145a7ec71
Juan Leyva <leyva.calle1403@gmail.com>
2022-09-22 00:32:01 -0500

    added more pages
    yessir

--- administrative_front/src/app/body/pages/solicitud/solicitud.component.ts
+++ administrative_front/src/app/body/pages/solicitud/solicitud.component.ts
@@ -2,10 +2,14 @@ import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { SenderService } from 'src/app/sender.service';
 
 export interface Soli {
   id: string;
   name: string;
+  rol: string;
+  desc: string;
   status: string;
   category: string;
   grade: string;
@@ -40,6 +44,11 @@ const CATEGORY: string[] = [
   'Astrologia'
 ];
 
+const ROLE: string[] = [
+  'Administrador',
+  'Profesor'
+];
+
 const GRADE: string[] = [
   '1ro',
   '2do',
@@ -62,7 +71,8 @@ export class SolicitudComponent implements AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor() { 
+  constructor(private router: Router, private service: SenderService) { 
+    
     const users = Array.from({length: 100}, (_, k) => createNewSoli(k + 1));
 
     // Assign the data to the data source for the table to render
@@ -83,6 +93,14 @@ export class SolicitudComponent implements AfterViewInit {
     }
   }
 
+  testing(aux: Soli){
+    if(aux.status == "Por confirmar"){
+      this.service.data = aux;
+      this.router.navigate(["/solicitud/details"])
+    }
+    else alert("Solo se puede detallar las solicitudes por confirmar")
+  }
+
 }
 
 function createNewSoli(id: number): Soli {
@@ -90,6 +108,8 @@ function createNewSoli(id: number): Soli {
   return {
     id: id.toString(),
     name: NAME[Math.round(Math.random() * (NAME.length - 1))],
+    rol: ROLE[Math.floor(Math.random() * (NAME.length - 1))],
+    desc: "Lorem Ipsum",
     status: STATUS[Math.floor(Math.random() * (NAME.length - 1))],
     category: CATEGORY[Math.round(Math.random() * (CATEGORY.length - 1))],
     grade: GRADE[Math.round(Math.random() * (GRADE.length - 1))],
--- administrative_front/src/app/body/subpages/see-request-detail/see-request-detail.component.ts
+++ administrative_front/src/app/body/subpages/see-request-detail/see-request-detail.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { SenderService } from 'src/app/sender.service';
+
+export interface Request {
+  id: string;
+  name: string;
+  rol: string;
+  desc: string;
+  status: string;
+  category: string;
+  grade: string;
+}
 
 @Component({
   selector: 'app-see-request-detail',
@@ -7,9 +18,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SeeRequestDetailComponent implements OnInit {
 
-  constructor() { }
+  detailService!: Request;
+
+  constructor(private service: SenderService) { }
 
   ngOnInit(): void {
+    this.detailService = this.service.data
+    console.log(this.detailService.name)
   }
+
+  
 
 }
